53c30e585431622520a7f20e14f7020117ea5b05
Thomas Wildeboer <thomaswildeboer@gmail.com>
2024-02-26 22:00:22 -0500

    Website

---
+++ site/src/persona.js
@@ -0,0 +1,39 @@
+export const persona = {
+    firstName: "Thomas",
+    secondName: "Wildeboer",
+    description: "computation and evolution",
+    email: "thomas.wildeboer@mail.utoronto.ca",
+    story: "I develop computational approaches to answer questions in evolutionary genetics as a graduate student at the University of Toronto. Primary foci are bridging between micro- and macroevolutionary theory and the use of large-scale individual-based simulations to understand the evolution of complex traits. I am also generally interested in software development and high performance computing.",
+    sections: {
+      /*publications: [
+          {
+              title: "google scholar",
+              desc: "see my research",
+              url: 'https://www.linkedin.com/in/thomas-wildeboer'
+          }
+      ],
+      projects: [
+        {
+            title: "genovolve",
+            desc: "evolutionary quantitative genetic simulator",
+            url: 'https://github.com/Thomas-Wildeboer/genovolve'
+        }
+      ],*/
+      affiliations: [
+        {
+            title: "parins-fukuchi lab",
+            url: 'https://tomopfuku.github.io/'
+        },
+        {
+            title: "sztepanacz lab",
+            url: 'https://sztepanacz.eeb.utoronto.ca/'
+        }
+      ]
+    },
+      contacts: [
+      {
+        title: "linkedin",
+        url: "https://linkedin.com/in/thomas-wildeboer",
+      }
+    ],
+  };
