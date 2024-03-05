f70f8eda783d2062311169b6b62eb456048616db
Thomas Wildeboer <thomaswildeboer@gmail.com>
2024-03-04 20:19:05 -0500

    More links

--- src/persona.js
+++ src/persona.js
@@ -5,6 +5,12 @@ export const persona = {
     email: "thomas.wildeboer@mail.utoronto.ca",
     story: "I develop computational approaches to answer questions in evolutionary genetics as a graduate student at the University of Toronto. Primary foci are bridging between micro- and macroevolutionary theory and the use of large-scale, individual-based simulations to understand the evolution of complex traits. I am also generally interested in software development and high performance computing.",
     sections: {
+      links: [
+        {
+          title: 'github',
+          url: 'https://github.com/thomas-wildeboer'
+        }
+      ],
       /*publications: [
           {
               title: "google scholar",
@@ -27,6 +33,10 @@ export const persona = {
         {
             title: "sztepanacz lab",
             url: 'https://sztepanacz.eeb.utoronto.ca/'
+        },
+        {
+            title: "eeb @ utoronto",
+            url: 'https://eeb.utoronto.ca/'
         }
       ]
     },
